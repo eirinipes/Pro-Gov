e40b45cedb20bc0eba692fe5834d8579df1b8fc8
DimitraVoutsa <t8240019@aueb.gr>
2025-11-21 15:51:42 +0200

    allages sta changes

--- Changes.java
+++ Changes.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
-public class Changes extends Esoda1 {
+public class Changes {
+    public static String process(String[] A, int[] B) {
+        System.out.println("Ποια κατηγορία θα ήθελες να αλλάξεις;");
+        for (int i = 0 ; i < A.lenght ; i++ ) {
+                System.out.println((i+1) + A[i]);
+            }
+            int katigoria = scanner.nextInt();
+            System.out.println(A[katigoria-1] + "-> ");
+            int timi = scanner.nextInt();
+            B[katigoria-1] = timi;
+            System.out.println(A[katigoria-1] + ":" + B[katigoria-1]);
+
+    }
     do {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Σε ποιον κλάδο θα ήθελες να κάνεις αλλαγές;");
